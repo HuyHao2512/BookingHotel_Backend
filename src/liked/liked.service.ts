1b76e0a8f93d0e1e15e3dddfc5806f28d4558443
HuyHao2512 <hohuyhao2013@gmail.com>
2025-08-10 20:18:28 +0700

    Refactor liked service: improve error messages, streamline property removal, and enhance liked properties retrieval

--- src/liked/liked.service.ts
+++ src/liked/liked.service.ts
@@ -14,43 +14,40 @@ export class LikedService {
     @InjectModel(Liked.name) private likedModel: Model<LikedDocument>,
   ) {}
 
-  // Thêm property vào danh sách yêu thích
   async addToLiked(createLikedDto: CreateLikedDto): Promise<Liked> {
     const { user, property } = createLikedDto;
 
-    // Kiểm tra xem user đã có danh sách yêu thích chưa
     const existingLike = await this.likedModel.findOne({ user });
 
     if (existingLike) {
-      // Kiểm tra xem property đã có trong danh sách yêu thích chưa
       if (existingLike.properties.includes(property.toString())) {
-        throw new BadRequestException(
-          'This property is already in your liked list',
-        );
+        throw new BadRequestException('Bạn đã lưu chỗ nghỉ này rồi');
       }
 
-      // Thêm property vào danh sách đã thích
       existingLike.properties.push(property.toString());
       return existingLike.save();
     }
 
-    // Nếu user chưa có danh sách yêu thích, tạo mới
     return this.likedModel.create({ user, properties: [property] });
   }
 
-  // Xóa property khỏi danh sách yêu thích
   async removeFromLiked(userId: string, propertyId: string): Promise<void> {
-    const deleted = await this.likedModel.findOneAndDelete({
-      user: userId,
-      property: propertyId,
-    });
-    if (!deleted) {
+    const updated = await this.likedModel.findOneAndUpdate(
+      { user: userId },
+      { $pull: { properties: propertyId } }, // gỡ propertyId ra khỏi mảng
+      { new: true },
+    );
+
+    if (!updated) {
       throw new NotFoundException('Property not found in your liked list');
     }
   }
 
-  // Lấy danh sách property đã thích của người dùng
   async getLikedProperties(userId: string) {
-    return this.likedModel.find({ user: userId });
+    const likedList = await this.likedModel
+      .find({ user: userId })
+      .populate('properties');
+
+    return likedList.flatMap((item) => item.properties);
   }
 }
